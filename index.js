20354cf8fee4dc54a6e154aa542af63e27ad3f26
BrianGacheru <briangacheru255@gmail.com>
2024-09-21 02:49:23 -0700

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let name= 'Aki'
+let hi='Hi, my name is '
+let language='JavaScript'
+function introduction(name) {
+    return `${hi}${name}.`
+}
+function  introductionWithLanguage(name,language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+function  introductionWithLanguageOptional(name,language='JavaScript.'){
+    return `Hi, my name is ${name} and I am learning to program in ${language}`
+}
+function introductionWithLanguageOptional(name,language='JavaScript.'){
+    return `Hi, my name is ${name} and I am learning to program in ${language}`
+}
+function  introductionWithLanguageOptional(name, language='JavaScript'){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
